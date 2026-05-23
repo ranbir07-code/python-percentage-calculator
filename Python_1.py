573ccf34a016528d6d1aeb19fc78660460697824
ranbir07-code <ranbirsehjal@gmail.com>
2026-05-23 16:32:42 +0530

    Add files via upload

---
+++ Python_1.py
@@ -0,0 +1,115 @@
+print("HI!!!")
+print("I AM HERE TO CALCULATE YOUR PERCENTAGE IN YOUR ")
+x=int(input("Enter total number of exam given:"))
+y=int(input("Enter maximum marks:"))
+if x==9:
+    q=int(input("Enter marks obtained in 1st exam:"))
+    w=int(input("Enter marks obtained in 2nd exam:"))
+    e=int(input("Enter marks obtained in 3rd exam:"))
+    r=int(input("Enter marks obtained in 4th exam:"))
+    t=int(input("Enter marks obtained in 5th exam:"))
+    m=int(input("Enter marks obtained in 6th exam:"))
+    u=int(input("Enter marks obtained in 7th exam:"))
+    i=int(input("Enter marks obtained in 8th exam:"))
+    o=int(input("Enter marks obtained in 9th exam:"))
+    Z=q+w+e+r+t+m+u+i+o
+    z=x*y
+    a=(Z/z*100)
+    print("Congratulations you got", Z, "out of", z)
+    print("Your percentage is ",a)
+if x==8:
+    q=int(input("Enter marks obtained in 1st exam:"))
+    w=int(input("Enter marks obtained in 2nd exam:"))
+    e=int(input("Enter marks obtained in 3rd exam:"))
+    r=int(input("Enter marks obtained in 4th exam:"))
+    t=int(input("Enter marks obtained in 5th exam:"))
+    m=int(input("Enter marks obtained in 6th exam:"))
+    u=int(input("Enter marks obtained in 7th exam:"))
+    i=int(input("Enter marks obtained in 8th exam:"))
+    Z=q+w+e+r+t+m+u+i
+    z=x*y
+    a=(Z/z*100)
+    print("Congratulations you got", Z, "out of", z)
+    print("Your percentage is ",a)
+if x==7:
+    q=int(input("Enter marks obtained in 1st exam:"))
+    w=int(input("Enter marks obtained in 2nd exam:"))
+    e=int(input("Enter marks obtained in 3rd exam:"))
+    r=int(input("Enter marks obtained in 4th exam:"))
+    t=int(input("Enter marks obtained in 5th exam:"))
+    m=int(input("Enter marks obtained in 6th exam:"))
+    u=int(input("Enter marks obtained in 7th exam:"))
+    Z=q+w+e+r+t+m+u
+    z=x*y
+    a=(Z/z*100)
+    print("Congratulations you got", Z, "out of", z)
+    print("Your percentage is ",a)
+if x==6:
+    q=int(input("Enter marks obtained in 1st exam:"))
+    w=int(input("Enter marks obtained in 2nd exam:"))
+    e=int(input("Enter marks obtained in 3rd exam:"))
+    r=int(input("Enter marks obtained in 4th exam:"))
+    t=int(input("Enter marks obtained in 5th exam:"))
+    m=int(input("Enter marks obtained in 6th exam:"))
+    Z=q+w+e+r+t+m
+    z=x*y
+    a=(Z/z*100)
+    print("Congratulations you got", Z, "out of", z)
+    print("Your percentage is ",a)
+if x==5:
+    q=int(input("Enter marks obtained in 1st exam:"))
+    w=int(input("Enter marks obtained in 2nd exam:"))
+    e=int(input("Enter marks obtained in 3rd exam:"))
+    r=int(input("Enter marks obtained in 4th exam:"))
+    t=int(input("Enter marks obtained in 5th exam:"))
+    Z=q+w+e+r+t
+    z=x*y
+    a=(Z/z*100)
+    print("Congratulations you got", Z, "out of", z)
+    print("Your percentage is ",a)
+if x==4:
+    q=int(input("Enter marks obtained in 1st exam:"))
+    w=int(input("Enter marks obtained in 2nd exam:"))
+    e=int(input("Enter marks obtained in 3rd exam:"))
+    r=int(input("Enter marks obtained in 4th exam:"))
+    Z=q+w+e+r
+    z=x*y
+    a=(Z/z*100)
+    print("Congratulations you got", Z, "out of", z)
+    print("Your percentage is ",a)
+if x==3:
+    q=int(input("Enter marks obtained in 1st exam:"))
+    w=int(input("Enter marks obtained in 2nd exam:"))
+    e=int(input("Enter marks obtained in 3rd exam:"))
+    Z=q+w+e
+    z=x*y
+    a=(Z/z*100)
+    print("Congratulations you got", Z, "out of", z)
+    print("Your percentage is ",a)
+if x==2:
+    q=int(input("Enter marks obtained in 1st exam:"))
+    w=int(input("Enter marks obtained in 2nd exam:"))
+    Z=q+w
+    z=x*y
+    a=(Z/z*100)
+    print("Congratulations you got", Z, "out of", z)
+    print("Your percentage is ",a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
